6116e03c0a93570729a710bdcc01f347b792f389
Jake Romer <jkrmr.io@gmail.com>
2017-08-06 16:37:38 -0400

    Add tests for employee_salary

---
+++ tests/employee_salary_test.py
@@ -0,0 +1,63 @@
+import json
+
+from models import employee_salary as Salary
+
+
+class Test_average_current_salaries_as_json(object):
+    def test_correctly_computes_avg_salary_by_dept(self):
+        employees = [{
+            'date': '2015-01-01',
+            'employee': 2,
+            'salary': 15,
+            'dept': 'Engineering'
+        }, {
+            'date': '2015-01-01',
+            'employee': 4,
+            'salary': 20,
+            'dept': 'Engineering'
+        }, {
+            'date': '2015-02-01',
+            'employee': 3,
+            'salary': 35,
+            'dept': 'Design'
+        }, {
+            'date': '2015-02-01',
+            'employee': 5,
+            'salary': 25,
+            'dept': 'Design'
+        }]
+        mean_salaries = Salary.average_current_salaries_as_json(employees)
+
+        expected = {'Engineering': 17.5, 'Design': 30}
+        assert expected == json.loads(
+            mean_salaries), '\nActual: %s\nExpected: %s' % (mean_salaries,
+                                                            expected)
+
+    def test_only_counts_an_employees_current_position(self):
+        employees = [{
+            'date': '2015-01-01',
+            'employee': 2,
+            'salary': 15,
+            'dept': 'Engineering'
+        }, {
+            'date': '2015-01-15',
+            'employee': 2,
+            'salary': 20,
+            'dept': 'Design'
+        }, {
+            'date': '2015-02-01',
+            'employee': 3,
+            'salary': 35,
+            'dept': 'Design'
+        }, {
+            'date': '2015-02-15',
+            'employee': 3,
+            'salary': 25,
+            'dept': 'Sales'
+        }]
+        mean_salaries = Salary.average_current_salaries_as_json(employees)
+
+        expected = {'Design': 20, 'Sales': 25}
+        assert expected == json.loads(
+            mean_salaries), '\nActual: %s\nExpected: %s' % (mean_salaries,
+                                                            expected)
