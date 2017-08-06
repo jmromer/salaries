e1fb7ee59773fa3ccbe7eabadf76845808aa473f
Jake Romer <jkrmr.io@gmail.com>
2017-08-06 13:25:27 -0400

    Extract stubbed headcount module

---
+++ employee_headcount.py
@@ -0,0 +1,2 @@
+def monthly_headcount_as_json(employee_list, department=None):
+    return department
--- simple_api.py
+++ simple_api.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request
 
+import employee_headcount as Headcount
 import employee_salary as Salary
 from data import employees as database
 
@@ -21,8 +22,9 @@ def averages():
 
 @app.route('/headcount_over_time', methods=['GET'])
 def headcount_over_time():
-    dept = request.args.get('department')
-    return dept
+    dept = request.args.get('department', '')
+    employees = database.all_employees
+    return Headcount.monthly_headcount_as_json(employees, dept)
 
 
 if __name__ == '__main__':
